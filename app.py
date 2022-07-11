1c2d3c778a2b1a23b4d4159649aa4fcd0207a55b
Terrias Bailey <90289918+terriasb@users.noreply.github.com>
2022-07-11 17:11:06 -0500

    Story program. Beginner python program.

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Hello World")
+character_name = "John"
+character_age = "69"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + ", ")
+print("He really liked the name " + character_name + ".")
+print("but didn't like being " + character_age + ".")
