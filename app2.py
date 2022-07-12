96d3a2639d44063a637902402e609169b89f0b91
Terrias Bailey <TerriasBailey@my.unt.edu>
2022-07-11 21:33:51 -0500

    String Application Py Program

--- app2.py
+++ app2.py
@@ -1,2 +1,13 @@
-print(" Giraffe\n \"Academy\"")  # The \ adds literal elements like a new line
+print(" \"Giraffe\"\n \"Academy\"")  # The \ adds literal elements like a new line
+phrase = "THIS IS A LOWERCASE SENTENCE"
+print(phrase.lower())
+print("This phrase is uppercase (t/f): ")
+print(phrase.islower())
+print("This phrase has this many characters:")
+print(len(phrase))
+print("The first character in this phrase is:")
+print(phrase[0])
+print("The first \"A\" appears at the index: ")
+print(phrase.index("A"))
+print(phrase.replace("LOWERCASE", "UPPERCASE"))
 
