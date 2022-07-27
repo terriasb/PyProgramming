c05712b23ffc775fb3d0ca32357a519c21827510
Terrias Bailey <TerriasBailey@my.unt.edu>
2022-07-27 11:36:05 -0500

    Classes

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Student:
+
+    def __init__(self, name, major, gpa, probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.probation = probation
---
+++ studentfile.py
@@ -0,0 +1,5 @@
+from classes import Student
+
+student1 = Student("Timmy", "Engineer", 2.33, False)
+
+print(student1.probation)
